24a6170fe26f03b1ea009e040eb8d21074e17102
str1fe <paal.cloud@gmail.com>
2013-08-02 21:30:07 +0200

    Created initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Howhard::Application.config.secret_key_base = 'a17438c4d9e6259abb7917898bc8ca4579f3037cd7241787627142b315f51658ca544e5a12e205e50497b5a390e99ea44174ed761f6c897890ab81c60152ab7e'
