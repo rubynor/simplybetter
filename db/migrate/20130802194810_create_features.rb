09d80a95464bdeea700722902f6c2013f4144001
str1fe <paal.cloud@gmail.com>
2013-08-02 21:49:04 +0200

    Created model feature and set up pg

---
+++ db/migrate/20130802194810_create_features.rb
@@ -0,0 +1,11 @@
+class CreateFeatures < ActiveRecord::Migration
+  def change
+    create_table :features do |t|
+      t.string :title
+      t.text :description
+      t.integer :creator_id
+
+      t.timestamps
+    end
+  end
+end
