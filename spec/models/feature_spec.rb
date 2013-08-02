1973b07c253cfdf41cb331882238fab338deb635
str1fe <paal.cloud@gmail.com>
2013-08-03 00:38:38 +0200

    Moved old spec file

--- spec/models/feature_spec.rb
+++ spec/models/feature_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Feature do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have a title' do
+    f = Feature.new(description: "Hey!")
+    expect{ f.save! }.to raise_error ActiveRecord::RecordInvalid, /title/i
+  end
+  it 'should have a body' do
+    f = Feature.new(title: "hey")
+    expect{ f.save! }.to raise_error ActiveRecord::RecordInvalid, /description/i
+  end
 end
