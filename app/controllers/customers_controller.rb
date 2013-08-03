1e85422fc654790a6bb7aa0dc00839800708e742
Bjørn Trondsen <bjorn@arctodus.no>
2013-08-03 11:11:28 +0200

    customer homepage with application form

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,10 @@
 class CustomersController < ApplicationController
+  before_action :authorize
+
+  def index
+    @applications = current_customer.applications
+  end
+
   def new
     @customer = Customer.new
   end
@@ -9,9 +15,23 @@ class CustomersController < ApplicationController
     redirect_to features_path
   end
 
+  def create_application
+    current_customer.applications.create!(application_attributes)
+    redirect_to action: :index
+  end
+
   private
+
+  def application_attributes
+    params.require(:application).permit(:name)
+  end
 
   def customer_attributes
     params.require(:customer).permit(:name, :email, :password, :password_confirmation)
   end
+
+  def authorize
+    redirect_to login_path unless current_customer
+  end
+
 end
--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -69,6 +69,6 @@ class FeaturesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def feature_params
-      params.require(:feature).permit(:title, :description, :creator_id)
+      params.require(:feature).permit(:title, :description, :creator_id, :application_id)
     end
 end
