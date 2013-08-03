d27ec706055aed69fd545cf8accfd5745c6bb23b
Bjørn Trondsen <bjorn@arctodus.no>
2013-08-03 12:37:15 +0200

    update vote count after voting

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,6 +3,7 @@ class VotesController < ApplicationController
     application = Application.find_by_token(params[:token])
     feature = application.features.find(params[:feature_id])
     feature.votes.create!(value: params[:value], voter_email: params[:voter_email])
-    render nothing: true
+    feature.reload
+    render js: "$('#feature_#{feature.id} .votes').text('#{feature.votes_count}')"
   end
 end
