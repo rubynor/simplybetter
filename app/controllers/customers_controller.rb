9b2c8789dfd2376b604aed1fe130b0cf1b4558e5
Bjørn Trondsen <bjorn@arctodus.no>
2013-08-03 12:16:40 +0200

    dont require login on signup

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,5 @@
 class CustomersController < ApplicationController
-  before_action :authorize
+  before_action :authorize, except: [:new, :create]
 
   def index
     @applications = current_customer.applications
