c31b1be83842048223d4f0ed3ead756cb392ddf5
str1fe <paal.cloud@gmail.com>
2013-08-28 16:44:22 +0200

    Created controller actions for administrating feature group

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationsController < ApplicationController
   def administrate
-    @application = current_customer.applications.find(params[:id])
+    @application = current_application
   end
 
   def new_feature
@@ -18,6 +18,23 @@ class ApplicationsController < ApplicationController
     end
   end
 
+  def administrate_group
+    @features = current_application.features #TODO: exclude features in group
+    @feature_group = current_application.feature_group.features
+  end
+
+  def add_feature_request_to_group
+    @feature = get_feature
+    @feature.feature_group = current_application.feature_group
+    respond_to_js @feature
+  end
+
+  def remove_feature_request_from_group
+    @feature = get_feature
+    @feature.feature_group = nil
+    respond_to_js @feature
+  end
+
   private
 
   def current_application
@@ -27,5 +44,17 @@ class ApplicationsController < ApplicationController
 
   def feature_attributes
     params.require(:feature).permit(:title, :description)
+  end
+
+  def get_feature
+    current_application.features.find(params[:fid])
+  end
+
+  def respond_to_js(feature)
+    respond_to do |format|
+      if feature.save!
+        format.js {  }
+      end
+    end
   end
 end
