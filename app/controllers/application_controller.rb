4ca412802a84e27f8c0447a1c67a76caaae455de
str1fe <paal.cloud@gmail.com>
2013-08-28 15:04:21 +0200

    Created applications_controller. Moved actions from application to applications controller. Added permitted attributes for feature to applications_controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,24 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def administrate
-    @application = current_customer.applications.find(params[:id])
-  end
 
-  def new_feature
-    @feature = Feature.new
-  end
-
-  def create_feature
-    @feature = Feature.new(params[:feature])
-    @feature.application = current_application
-    @feature.creator = current_customer
-    respond_to do |format|
-      if @feature.save!
-        format.html { redirect_to application_new_feature_path(current_application.id), notice: 'Feature was successfully created.' }
-      end
-    end
-  end
 
   private
 
@@ -32,10 +15,5 @@ class ApplicationController < ActionController::Base
   def authorize
     redirect_to login_url, alert: "Not authorized" if current_customer.nil?
   end
-
-  def current_application
-    @current_application ||= current_customer.applications.find(params[:id])
-  end
-  helper_method :current_application
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Howhard::Application.routes.draw do
     end
   end
 
-  get 'applications/:id' => "application#administrate"
-  get 'applications/:id/feature/new' => "application#new_feature", as: "application_new_feature"
-  post 'applications/:id/feature/create' => "application#create_feature"
+  get 'applications/:id' => "applications#administrate"
+  get 'applications/:id/feature/new' => "applications#new_feature", as: "application_new_feature"
+  post 'applications/:id/feature/create' => "applications#create_feature"
 
   get '/comments/create' => "comments#create"
   get '/comments/destroy' => "comments#destroy"
