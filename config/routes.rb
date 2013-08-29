601af905df17f68e9921f90105d7fa66496e0826
str1fe <paal.cloud@gmail.com>
2013-08-29 11:13:36 +0200

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Howhard::Application.routes.draw do
   get 'applications/:id/feature/new' => "applications#new_feature", as: "application_new_feature"
   post 'applications/:id/feature/create' => "applications#create_feature"
   get 'applications/:id/administrate_group' => "applications#administrate_group", as: "administrate_group"
+  post 'applications/:id/add_request_to_group/:fid' => "applications#add_feature_request_to_group", as: "add_feature_request_to_group"
+  delete 'applications/:id/remove_request_from_group/:fid' => "applications#remove_feature_request_from_group", as: "remove_feature_request_from_group"
 
   get '/comments/create' => "comments#create"
   get '/comments/destroy' => "comments#destroy"
