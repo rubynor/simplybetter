d4b1a2b51a35212b99d9b10d8c5b431c6cecaf1e
str1fe <paal.cloud@gmail.com>
2013-09-11 14:12:53 +0200

    Added template for feature items. Now fetching it from the feature_view

--- app/assets/javascripts/backbone/views/feature_view.js
+++ app/assets/javascripts/backbone/views/feature_view.js
@@ -1,8 +1,21 @@
 
 var FeatureView = Backbone.View.extend({
+    template: function(){
+        var tmp = "";
+        $.ajax({
+            url: 'http://localhost:3000/assets/backbone/templates/feature.html',
+            method: 'GET',
+            async: false,
+            success: function(response){
+                tmp = response;
+            }
+        });
+        return tmp;
+    },
     tagName: 'li',
 
     render: function(){
-        return "<li>"+this.model.get('title')+"</li>";
+        var self = this;
+        return _.template(self.template(),this.model.attributes);
     }
 });
