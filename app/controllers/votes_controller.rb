b2f4527eca630f9a6943c8d4ad1ce51f80a266a1
str1fe <paal.cloud@gmail.com>
2013-09-11 15:04:16 +0200

    Updated votes_controller. Now a user can just vote one up, or one down

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,24 +1,55 @@
 class VotesController < ApplicationController
-  def cast
-    application = Application.find_by_token(params[:token])
+  def up
+    cast_vote(1)
+  end
+  def down
+    cast_vote(-1)
+  end
+
+  private
+
+  def cast_vote(value)
+    vote = vote_receiver.votes.find_or_initialize_by(voter_email: params[:voter_email])
+    vote.value = 0
+    vote.value += value
+    vote.save!
+  end
+
+  def find_application
+    Application.find_by_token(params[:token])
+  end
+
+  def vote_receiver
+    application = find_application
     if params[:feature_id]
-      vote_receiver = application.features.find(params[:feature_id])
+      application.features.find(params[:feature_id])
     elsif params[:comment_id]
-      vote_receiver = application.comments.find(params[:comment_id])
+      application.comments.find(params[:comment_id])
     else
       raise "No vote receiver"
     end
-
-    vote = vote_receiver.votes.find_or_initialize_by(voter_email: params[:voter_email])
-    vote.value ||= 0
-    vote.value += params[:value].to_i
-    vote.save!
-    vote_receiver.reload
-
-    @feature = vote_receiver.instance_of?(Comment)? vote_receiver.feature : vote_receiver
-
-    respond_to do |format|
-      format.json
-    end
   end
+
+  #def cast
+  #  application = Application.find_by_token(params[:token])
+  #  if params[:feature_id]
+  #    vote_receiver = application.features.find(params[:feature_id])
+  #  elsif params[:comment_id]
+  #    vote_receiver = application.comments.find(params[:comment_id])
+  #  else
+  #    raise "No vote receiver"
+  #  end
+  #
+  #  vote = vote_receiver.votes.find_or_initialize_by(voter_email: params[:voter_email])
+  #  vote.value ||= 0
+  #  vote.value += params[:value].to_i
+  #  vote.save!
+  #  vote_receiver.reload
+  #
+  #  @feature = vote_receiver.instance_of?(Comment)? vote_receiver.feature : vote_receiver
+  #
+  #  respond_to do |format|
+  #    format.json
+  #  end
+  #end
 end
