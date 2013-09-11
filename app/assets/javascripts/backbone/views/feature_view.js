588fa348d4592af4a6c7fc1954fea4401484c4c3
str1fe <paal.cloud@gmail.com>
2013-09-11 17:19:19 +0200

    Vote up and down functions, and made the render action return this - very important

--- app/assets/javascripts/backbone/views/feature_view.js
+++ app/assets/javascripts/backbone/views/feature_view.js
@@ -1,5 +1,5 @@
 
-var FeatureView = Backbone.View.extend({
+var FeatureItemView = Backbone.View.extend({
     template: function(){
         var tmp = "";
         $.ajax({
@@ -13,9 +13,37 @@ var FeatureView = Backbone.View.extend({
         return tmp;
     },
     tagName: 'li',
+    events: {
+        "click .up" : "vote_up",
+        "click .down" : "vote_down"
+    },
+
+    vote: function(upOrDown,element){
+        var self = this;
+        var id = this.model.get('id');
+        var element = $(element.target).closest('li');
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:3000/'+upOrDown,
+            success: function(response){
+                element.html(self.render());
+            }
+        });
+    },
+    vote_up: function(e){
+        console.log("You triggered vote_up!");
+        console.log("I am "+ this.model.get('id'));
+        this.vote('vote_up',e);
+    },
+    vote_down: function(e){
+        console.log("You triggered vote_down!");
+        console.log("I am "+ this.model.get('id'));
+        this.vote('vote_down',e);
+    },
 
     render: function(){
         var self = this;
-        return _.template(self.template(),this.model.attributes);
+        this.$el.addClass('feature').html(_.template(self.template(),this.model.attributes));
+        return this;
     }
 });
