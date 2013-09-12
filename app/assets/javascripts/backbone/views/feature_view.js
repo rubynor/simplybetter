e091e2ec0bfcdf03c95e0a8881556478fc416a5b
str1fe <paal.cloud@gmail.com>
2013-09-12 10:34:50 +0200

    Now re-rendering feature after casting a vote

--- app/assets/javascripts/backbone/views/feature_view.js
+++ app/assets/javascripts/backbone/views/feature_view.js
@@ -24,21 +24,31 @@ var FeatureItemView = Backbone.View.extend({
         var element = $(element.target).closest('li');
         $.ajax({
             type: 'POST',
-            url: 'http://localhost:3000/'+upOrDown,
+            url: 'http://localhost:3000/vote_'+upOrDown,
+            data: {
+                token: window.appKey,
+                voter_email: window.userEmail,
+                voter_name: window.userName,
+                feature_id: id
+            },
             success: function(response){
-                element.html(self.render());
+                self.model.fetch({
+                    success: function(){
+                        self.render();
+                    }
+                });
             }
         });
     },
     vote_up: function(e){
         console.log("You triggered vote_up!");
         console.log("I am "+ this.model.get('id'));
-        this.vote('vote_up',e);
+        this.vote('up',e);
     },
     vote_down: function(e){
         console.log("You triggered vote_down!");
         console.log("I am "+ this.model.get('id'));
-        this.vote('vote_down',e);
+        this.vote('down',e);
     },
 
     render: function(){
