068566af4f3a051e96aec903f2a13d86879f8c20
str1fe <paal.cloud@gmail.com>
2013-09-12 17:06:10 +0200

    Configs are now set when we initialize the app

--- app/assets/javascripts/setup.js
+++ app/assets/javascripts/setup.js
@@ -2,17 +2,26 @@ var SimplyBetterApplication = (function (app){
     return app;
 }(SimplyBetterApplication || {}));
 
-SimplyBetterApplication.config = (function (){
-    var app = {};
-    // Add configurations here
-    app.baseUrl = 'http://localhost:3000'
-    return app;
-}());
 
 SimplyBetterApplication.init = (function (){
     var app = {};
     // Add init actions here
-    app.show = function(){
+    app.start = function(appKey){
+        // Set configurations on start of app
+        SimplyBetterApplication.config = (function (key){
+            var app = {};
+            // Add configurations here
+            app.baseUrl = 'http://localhost:3000';
+            app.featuresCollectionUrl = function(){
+                return '/features?token=' + key;
+            };
+            app.featuresModelUrl = function(id){
+                return '/features/' + id + '?token=' + key;
+            };
+            return app;
+        }(appKey));
+
+        // Create collection, fetch, and render
         var col = new SimplyBetterApplication.Features.collection();
         var cv = new SimplyBetterApplication.Features.collectionView({collection: col});
         col.fetch({
@@ -20,6 +29,7 @@ SimplyBetterApplication.init = (function (){
                 cv.render();
             }
         });
+        // \Create collection, fetch, and render
     };
 
     return app;
