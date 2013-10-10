cee2e321cfb1a4dfd3c6fc56214dab68723b8b15
str1fe <paal.cloud@gmail.com>
2013-10-10 18:12:55 +0200

    Updated routes and set new path variables

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ SimplyBetter::Application.routes.draw do
       post :down, on: :collection
     end
   end
+
   resources :comments, only: :destroy
   resources :customers, only: [:index, :new, :create] do
     member do
@@ -17,19 +18,16 @@ SimplyBetter::Application.routes.draw do
     end
   end
 
-  resources :applications, only: [:index, :create]
+  resources :applications, only: [:index, :create, :show] do
+    get :administrate_group, on: :member
+    resources :features, except: [:show, :index] do
+      post :add_to_group, on: :member
+      delete :remove_from_group, on: :member
+    end
+  end
 
   get 'widget' => "widget#widget"
 
-  get 'applications/:id' => "applications#administrate", as: "application_administrate"
-  get 'applications/:id/feature/new' => "applications#new_feature", as: "application_new_feature"
-  post 'applications/:id/feature/create' => "applications#create_feature"
-  get 'applications/:id/features/:fid/edit' => "applications#edit_feature", as: "applications_edit_feature"
-  patch 'applications/:id/features/:fid' => "applications#update_feature"
-  delete 'applications/:id/features/:fid' => "applications#delete_feature", as: "applications_delete_feature"
-  get 'applications/:id/administrate_group' => "applications#administrate_group", as: "administrate_group"
-  post 'applications/:id/add_request_to_group/:fid' => "applications#add_feature_request_to_group", as: "add_feature_request_to_group"
-  delete 'applications/:id/remove_request_from_group/:fid' => "applications#remove_feature_request_from_group", as: "remove_feature_request_from_group"
 
   get '/comments/create' => "comments#create"
   get '/comments/destroy' => "comments#destroy"
