8498ec2a40f7da7020655e5a851836d4aade4681
Pål Andre Sundt <paal.cloud@gmail.com>
2014-02-11 12:31:50 +0100

    Update rake

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140210131621) do
+ActiveRecord::Schema.define(version: 20140211112619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140210131621) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "intro"
+    t.string   "icon"
   end
 
   create_table "comments", force: true do |t|
