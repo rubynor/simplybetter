324ac863348d88e582dc4da1bb0a8c16b3c2493d
Pål Andre Sundt <paal.cloud@gmail.com>
2014-02-19 10:46:27 +0100

    Other customers also has to be recognized and authenticated

--- app/controllers/concerns/creator_finder.rb
+++ app/controllers/concerns/creator_finder.rb
@@ -1,9 +1,12 @@
 module CreatorFinder
   def creator(application, creator_email)
     customer = application.customer
+    a_customer = Customer.find_by(email: creator_email)
     user = User.find_by(email: creator_email)
     if customer.email == creator_email
       creator = customer
+    elsif a_customer
+      creator = a_customer
     elsif user
       creator = user
     else
