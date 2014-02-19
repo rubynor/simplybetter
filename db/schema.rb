c0da15f6e239256fb9cf83979ad2ef69c8288dcf
Pål Andre Sundt <paal.cloud@gmail.com>
2014-02-19 10:33:11 +0100

    Removed column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212090548) do
+ActiveRecord::Schema.define(version: 20140212144634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,7 +82,6 @@ ActiveRecord::Schema.define(version: 20140212090548) do
   create_table "votes", force: true do |t|
     t.integer  "vote_receiver_id"
     t.string   "vote_receiver_type"
-    t.string   "voter_email"
     t.integer  "value"
     t.datetime "created_at"
     t.datetime "updated_at"
