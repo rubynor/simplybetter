6838128975734468ed5ef4ce0067b4004401146c
Pål Andre Sundt <paal.cloud@gmail.com>
2014-02-20 10:44:35 +0100

    Set size of the content-container

--- app/assets/javascripts/widget/widget2.js
+++ app/assets/javascripts/widget/widget2.js
@@ -4,3 +4,16 @@
 //= require backbone
 //= require widget/setup
 //= require_tree ./backbone
+
+$(function(){
+    var resize = function(){
+        $('#simplybetterIdeasModalContent')
+            .height($(window).height() - $('#simplybetterNavbar').height());
+    }
+    $(window).on('resize', function(){
+        resize();
+    });
+    setTimeout(function(){
+        resize();
+    },2000);
+});
