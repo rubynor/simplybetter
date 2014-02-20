95e71c35ea2d008724c0876021759bd7d844745a
Pål Andre Sundt <paal.cloud@gmail.com>
2014-02-20 10:47:29 +0100

    Add explanation

--- app/assets/javascripts/widget/widget2.js
+++ app/assets/javascripts/widget/widget2.js
@@ -6,6 +6,9 @@
 //= require_tree ./backbone
 
 $(function(){
+  // It is not possible to have a dynamic navbar and a flexible
+  // div beneath that stretches to the bottom and uses overflow-y: scroll
+  // We will set the height with javascript
     var resize = function(){
         $('#simplybetterIdeasModalContent')
             .height($(window).height() - $('#simplybetterNavbar').height());
