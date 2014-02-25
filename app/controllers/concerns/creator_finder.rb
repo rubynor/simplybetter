10a7f0bfc185c6814fb21e3293b740467ae15187
Pål Andre Sundt <paal.cloud@gmail.com>
2014-02-25 13:40:36 +0100

    Add alias for customer. Removed bug causing customer to return the wrong customer

--- app/controllers/concerns/creator_finder.rb
+++ app/controllers/concerns/creator_finder.rb
@@ -1,14 +1,22 @@
 module CreatorFinder
+  def get_current_user(application,user_email)
+    if application && user_email
+      @current_user ||= creator(application, user_email)
+    else
+      raise "Missing one of the arguments application: #{application}, or user_email: #{user_email}"
+    end
+  end
+
   def creator(application, creator_email)
     customer = application.customer
     a_customer = Customer.find_by(email: creator_email)
     user = User.find_by(email: creator_email)
     if customer.email == creator_email
-      creator = customer
+      return customer
     elsif a_customer
-      creator = a_customer
+      return a_customer
     elsif user
-      creator = user
+      return user
     else
       raise "This user does not exist"
     end
