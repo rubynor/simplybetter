7859cd82d0bc0f2d01f64520d2f073387f6c3bea
Pål Andre Sundt <paal.cloud@gmail.com>
2014-03-02 23:45:04 +0100

    We need this

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,9 @@ SimplyBetter::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+
+  config.react.variant = :production
+
   class WidgetPath
     def self.path
       "http://www.simplybetter.io"
