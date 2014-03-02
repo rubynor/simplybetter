b61542d8b65a9f74cbd527bcc127e84f3df90c2b
Pål Andre Sundt <paal.cloud@gmail.com>
2014-03-02 15:10:58 +0100

    Add nested model comments to idea

--- app/assets/javascripts/backbone/dashboard_ideas/models/idea_model.js
+++ app/assets/javascripts/backbone/dashboard_ideas/models/idea_model.js
@@ -1,10 +1,31 @@
 SimplyBetterIdeas.Models = (function(models){
   var module = models;
 
+  module.Comment = Backbone.Model.extend({
+    
+  });
+
+  module.CommentCollection = Backbone.Collection.extend({
+    constructor: function(models,options){
+      this.idea = options.idea;
+      Backbone.Collection.apply(this, arguments);
+    },
+    model: module.Comment
+  });
+
   module.Idea = Backbone.Model.extend({
     urlRoot: function(){
       return '/applications/' + SimplyBetterIdeas.Config.appId + '/ideas'
+    },
+    parse: function(data){
+      var that = this;
+      if (data.comments.length > 0){
+        this.commentCollection = new module.CommentCollection(data.comments,{idea: that});
+      }
+     delete data['comments'];
+     return data;
     }
+
   });
   module.IdeaCollection = Backbone.Collection.extend({
     url: function(){
@@ -12,5 +33,6 @@ SimplyBetterIdeas.Models = (function(models){
     },
     model: module.Idea
   });
+
   return module;
 })(SimplyBetterIdeas.Models || {});
