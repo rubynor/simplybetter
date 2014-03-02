b0fd475e0271322a8560a9809431c0703ea6a078
Pål Andre Sundt <paal.cloud@gmail.com>
2014-03-02 15:11:45 +0100

    Add routes with filters to router

--- app/assets/javascripts/backbone/dashboard_ideas/routers/ideas_router.js.jsx
+++ app/assets/javascripts/backbone/dashboard_ideas/routers/ideas_router.js.jsx
@@ -4,22 +4,55 @@ SimplyBetterIdeas.Routers = (function(router){
 
   module.Ideas = Backbone.Router.extend({
     routes: {
-      '': 'all'
+      '': 'all',
+      'all': 'all',
+      'hidden': 'hidden',
+      'visible': 'visible'
+    },
+
+    render: function(collection){
+      var view = SimplyBetterIdeas.Views.Ideas;
+      React.renderComponent((
+        <view myCollection={collection} />
+      ), document.getElementById('manage-ideas'));
+    },
+
+    fetchIdeas: function(filter){
+      var that = this;
+      var ideas = new SimplyBetterIdeas.Models.IdeaCollection();     
+      console.log(ideas);
+      ideas.fetch({
+        success: function(collection){
+          if (filter){
+            that.render(filter(collection));
+          } else {
+            that.render(collection);
+          }
+        }
+      });
+    },
+
+    filterWhereCondition: function(condition){
+      var filter = function(collection){
+        return collection.where(condition);
+      };
+      this.fetchIdeas(filter);
     },
 
     all: function(){
-      var ideas = new SimplyBetterIdeas.Models.IdeaCollection();     
-      var view = SimplyBetterIdeas.Views.Ideas;
-      ideas.fetch({
-        success: function(collection){
-          React.renderComponent((
-            <view myCollection={collection} />
-          ), document.getElementById('manage-ideas'));
+      this.fetchIdeas();
+    },
+    
+    visible: function(){
+      this.filterWhereCondition({idea_group_id: 1})
+    },
 
-        }
-      });
+    hidden: function(){
+      this.filterWhereCondition({idea_group_id: null})
     }
+
   });
+
 
   return module;
 })(SimplyBetterIdeas.Routers || {});
