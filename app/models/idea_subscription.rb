cadfa26c12478bdd44ef2e12c0557aa1e5dd3e20
Pål Andre Sundt <paal.cloud@gmail.com>
2014-03-05 16:16:13 +0100

    Add inverse of relation to idea

--- app/models/idea_subscription.rb
+++ app/models/idea_subscription.rb
@@ -1,5 +1,5 @@
 class IdeaSubscription < ActiveRecord::Base
-  belongs_to :idea
+  belongs_to :idea, inverse_of: :subscriptions
   belongs_to :subscriber, polymorphic: true
   belongs_to :subscriber_from, polymorphic: true
 end
