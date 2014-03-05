0192727bf30cc840baedcab802db403d84874939
Pål Andre Sundt <paal.cloud@gmail.com>
2014-03-05 16:15:46 +0100

    Add some helpful methods

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,12 +3,22 @@ class Notification < ActiveRecord::Base
   belongs_to :action, polymorphic: true
   belongs_to :recipient, polymorphic: true
 
+  def self.notify(action, subject)
+    subject.subscribers.each do |s|
+      create(action: action, subject: subject, recipient: s)
+    end
+  end
+
+  def self.for(recipient)
+    all.where(recipient: recipient)
+  end
+
   def action_user_image
     self.action.creator.gravatar_url
   end
 
   def text
-    self.action.notification_text
+    self.action.notification_text(recipient)
   end
 
   def time
