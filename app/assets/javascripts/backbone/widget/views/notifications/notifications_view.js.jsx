8aa7d18b19b74f85e88be59791c2e6be3e732453
Pål Andre Sundt <pal@rubynor.com>
2014-03-11 20:18:19 +0100

    Add 'Go to notification subject'-support

--- app/assets/javascripts/backbone/widget/views/notifications/notifications_view.js.jsx
+++ app/assets/javascripts/backbone/widget/views/notifications/notifications_view.js.jsx
@@ -54,11 +54,20 @@ SimplyBetterApplication.Views = (function(views){
       else
         this.setState({show: true});
     },
+    hide: function(){
+      this.setState({show: false});
+    },
     renderNotifications: function(){
       var notification = module.Notification;
+      var navigator = this.props.navigator;
+      var that = this;
       var notifications = this.props.collection.models.map(function(model){
         return (
-          <notification key={model.cid} model={model} />
+          <notification 
+            key={model.cid} 
+            model={model} 
+            navigator={navigator}
+            parent={that} />
         );
       });
       return notifications;
@@ -103,10 +112,28 @@ SimplyBetterApplication.Views = (function(views){
       });
       return textElements;
     },
+    goTo: function(){
+      var model = this.props.model;
+      this.props.navigator.trigger(
+        'notification',
+        {
+          id: model.get('idea_id'),
+          highlight: function(){
+            if (model.get('comment_id'))
+              return {comment: model.get('comment_id')};
+            else
+              return {idea: model.get('idea_id')};
+          }()
+        }
+      );
+      this.props.parent.hide();
+    },
     render: function(){
       var attr = this.props.model.attributes;
       return (
-        <div className={this.notificationClass()}>
+        <div 
+          className={this.notificationClass()}
+          onClick={this.goTo}>
           <div className='profile-image'>
             <img src={attr.image_url} />
           </div>
