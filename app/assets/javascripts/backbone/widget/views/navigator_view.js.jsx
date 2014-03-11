c32848fb306bc4ebf2c2564d846edef9d969c981
Pål Andre Sundt <pal@rubynor.com>
2014-03-11 16:40:49 +0100

    Add jsx to navigator view to support react

---
+++ app/assets/javascripts/backbone/widget/views/navigator_view.js.jsx
@@ -0,0 +1,119 @@
+/** @jsx React.DOM */
+SimplyBetterApplication.Navigator = (function(navigator){
+    var module = navigator;
+
+    module.NavigatorView = Backbone.View.extend({
+        initialize: function(){
+            this.ideas = new SimplyBetterApplication.Ideas.collection();
+            this.ideasView = new SimplyBetterApplication.Ideas.collectionView({
+                collection: this.ideas, 
+                navigator: this
+            });
+            this.application = new SimplyBetterApplication.Application.Model();
+            this.listenTo(this,'close',this.deActivateLink);
+        },
+        template: 'navigator.html',
+        el: '#simplybetterIdeasModal',
+        events: {
+            "click .goToIdeas": "navigateOverview",
+            "click .home": "navigateToRootPage"
+        },
+
+        navigateOverview: function(e){
+            e.preventDefault();
+            this.trigger('close');
+            $(e.target).closest('#simplybetterNavbar').addClass('overview');
+            var newIdeaView = new SimplyBetterApplication.Ideas.newIdeaView({
+                navigator: this,
+                ideasCollection: this.ideas
+            });
+            var overviewLayout = new SimplyBetterApplication.Ideas.OverviewLayout({
+                newIdeaView: newIdeaView,
+                ideasView: this.ideasView,
+                navigator: this
+            })
+            this.$el
+                .find('#simplybetterIdeasModalContent')
+                .html(overviewLayout.render().el);
+            this.ideas.fetch();
+
+            var notifications = new SimplyBetterApplication.Notifications.Collection();
+            var notificationsView = SimplyBetterApplication.Views.Notifications;
+            notifications.fetch();
+            if ($('SimplybetterNotifications').length === 0){
+              React.renderComponent((
+                <notificationsView collection={notifications} />
+              ), document.getElementById('simplybetterNotifications'));
+            }
+        },
+
+        showIdea: function(ideaModel,voteModel,container){
+            if (!voteModel) {
+                var voteModel = new SimplyBetterApplication.Votes.Model({
+                    idea_id: ideaModel.get('id'), 
+                    voter_email: SimplyBetterApplication.config.userEmail
+                });
+                voteModel.fetch();
+            }
+            var idea_layout = new SimplyBetterApplication.Ideas.Layout({
+                idea: ideaModel, 
+                navigator: this, 
+                voteModel: voteModel
+            });
+
+            if (container){
+                this.$el
+                    .find(container)
+                    .html(idea_layout.render().el);
+            } else {
+                this.trigger('close');
+                this.$el
+                    .find('#simplybetterIdeasModalContent')
+                    .html(idea_layout.render().el);
+            }
+            return idea_layout;
+        },
+
+        alertSuccess: function(message){
+            var successView = new SimplyBetterApplication.Alerts.SuccessView({
+                message: message
+            });
+            successView.render();
+        },
+
+        closeModal: function(){
+            this.$el.hide();
+            this.$el.prev().hide();
+        },
+
+        navigateToRootPage: function(){
+          // Deprecated needs renewal
+            root_link = $('.goToIdeas');
+            if (!root_link.hasClass('active')) {
+                root_link.click();
+            }
+            return false;
+        },
+
+        deActivateLink: function(){
+            this.$el.find('#simplybetterNavbar.overview').removeClass('overview');
+        },
+
+        render: function() {
+            var self = this;
+            var template = SimplyBetterApplication.Template.get(this.template);
+            this.application.fetch({
+                success: function(model){
+                    self.$el.html(template({
+                      applicationName: model.get('name'),
+                      applicationIntro: model.get('intro')
+                    }));
+                    self.navigateToRootPage();
+                }
+            });
+        }
+
+    });
+
+    return module;
+}(SimplyBetterApplication.Navigator || {}));
