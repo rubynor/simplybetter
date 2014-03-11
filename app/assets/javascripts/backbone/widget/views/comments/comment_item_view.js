243eddf69be102c82f4fa11af332c2a394547198
Pål Andre Sundt <pal@rubynor.com>
2014-03-11 20:11:37 +0100

    Make comments highlightable

--- app/assets/javascripts/backbone/widget/views/comments/comment_item_view.js
+++ app/assets/javascripts/backbone/widget/views/comments/comment_item_view.js
@@ -5,7 +5,30 @@ SimplyBetterApplication.Comments = (function(comments){
         template: 'comments/comment_item.html',
         tagName: 'li',
         className: 'comment-item',
+        id: function(){
+          return this.model.get('id') + '-comment';
+        },
 
+        highlight: function(){
+          var that = this;
+          var type = this.options.highlight;
+          window.location.hash = '';
+          if (type['comment']){
+            if (this.model.get('id') === parseInt(type['comment'])){
+              setTimeout(function(){
+                that.$el.addClass('highlighted');
+                window.location.hash = that.id();
+              },500);
+              setTimeout(function(){
+                that.$el.removeClass('highlighted');
+              },3000);
+            }
+          }
+        },
+
+        afterRender: function(){
+          this.highlight();
+        }
     });
 
     return module;
