9a2b862cf02f4bac4c2502fe1c9e116a512961a7
Pål Andre Sundt <pal@rubynor.com>
2014-03-13 14:04:20 +0100

    Add support for notifications for attribute-changes in a model

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,16 +2,45 @@ class Notification < ActiveRecord::Base
   belongs_to :subject, polymorphic: true
   belongs_to :action, polymorphic: true
   belongs_to :recipient, polymorphic: true
+  belongs_to :action_attribute_changed_by, polymorphic: true
 
-  def self.notify(action, subject)
-    notify_group(subject.subscribers, action, subject)
+  validate :subject, presence: true
+  validate :action, presence: true
+  validate :recipient, presence: true
+
+  def self.notify(action, subject, action_attribute = nil, action_attribute_changed_by = nil)
+    notify_group(subject.subscribers, action, subject, action_attribute, action_attribute_changed_by)
   end
 
-  def self.notify_group(group, action, subject)
+  def self.notify_group(
+      group,
+      action,
+      subject,
+      action_attribute = nil,
+      action_attribute_changed_by = nil
+    )
+
     group.each do |u|
-      unless action.creator == u
-        create(action: action, subject: subject, recipient: u)
+      if should_get_notification(action,u,action_attribute_changed_by)
+        notification = new(
+          action: action,
+          subject: subject,
+          recipient: u,
+          action_attribute: action_attribute
+        )
+        if action_attribute_changed_by.present?
+          notification.action_attribute_changed_by = action_attribute_changed_by
+        end
+        notification.save!
       end
+    end
+  end
+
+  def self.should_get_notification(action,group_user,action_attribute_changed_by)
+    if action_attribute_changed_by.present?
+      group_user != action_attribute_changed_by
+    else
+      action.creator != group_user
     end
   end
 
@@ -34,7 +63,7 @@ class Notification < ActiveRecord::Base
   end
 
   def text
-    self.action.notification_text(recipient)
+    self.action.notification_text(recipient, action_attribute, action_attribute_changed_by)
   end
 
   def time
