e56adea013244511ec503f7f9aff32c8a629abc7
Pål Andre Sundt <pal@rubynor.com>
2014-03-18 16:02:31 +0100

    Createcontroller actions for edit, edit_unsafe, update, update_unsafe

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,7 @@
 class CustomersController < ApplicationController
   before_action :authorize, except: [:new, :create]
-  layout "splash_screen"
-
-  def index
-    @applications = current_customer.applications.includes(:ideas)
-  end
+  before_action :set_application, only: [:edit, :update, :edit_unsafe, :update_unsafe]
+  layout "splash_screen", except: [:edit, :update, :edit_unsafe, :update_unsafe]
 
   def new
     @customer = Customer.new
@@ -20,6 +17,31 @@ class CustomersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def edit_unsafe
+  end
+
+  def update
+    current_customer.update_attributes!(safe_attributes)
+    redirect_to :back, notice: 'Profile updated!'
+  end
+
+  def update_unsafe
+    attributes = unsafe_attributes
+    if current_customer.authenticate(attributes.delete(:current_password))
+      if current_customer.update_attributes(attributes)
+        redirect_to edit_unsafe_customer_path, notice: 'Profile updated!'
+      else
+        render :edit_unsafe
+      end
+    else
+      flash[:error] = 'Validation failed. Current password was incorrect'
+      redirect_to :back
+    end
+  end
+
   private
 
   def application_attributes
@@ -28,6 +50,14 @@ class CustomersController < ApplicationController
 
   def customer_attributes
     params.require(:customer).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def safe_attributes
+    params.require(:customer).permit(:name)
+  end
+
+  def unsafe_attributes
+    params.require(:customer).permit(:email, :password, :password_confirmation, :current_password)
   end
 
   def authorize
