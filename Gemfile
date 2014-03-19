ac36aef742223a71460d08db15fa3315d0fc406a
Pål Andre Sundt <pal@rubynor.com>
2014-03-19 10:57:38 +0100

    Bundle update

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', '4.0.0'
+gem 'rails', '~> 4.0.4'
 gem 'rack-cors', :require => 'rack/cors'
 
 gem 'pg'
--- app/assets/javascripts/sb_tools.js
+++ app/assets/javascripts/sb_tools.js
@@ -2,7 +2,7 @@ SBTools = (function(){
     var sb = {};
 
     sb.isPresent = function(jquerySelector){
-        return jquerySelector.length > 0 ? true : false
+        return jquerySelector.length ? true : false
     }
     
     return sb;
