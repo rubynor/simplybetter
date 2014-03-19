fdf30fd9b8e3a97bbd2e1c18e0f37549083f1724
Pål Andre Sundt <pal@rubynor.com>
2014-03-19 11:00:00 +0100

    Too much logging in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,9 +43,9 @@ SimplyBetter::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :debug
-  config.logger = Logger.new(STDOUT)
-  config.logger.level = Logger::DEBUG
+  #config.log_level = :debug
+  #config.logger = Logger.new(STDOUT)
+  #config.logger.level = Logger::DEBUG
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
