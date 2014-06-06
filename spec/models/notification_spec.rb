cab7a1bc9e678583f379bc83fbe12c9752af6ddf
Hilde Vestøl <hilde@rubynor.com>
2014-06-06 18:12:15 +0200

    update spec

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -8,20 +8,20 @@ describe Notification do
 
     it 'should create a notification' do
       expect {
-        Notification.create_with(action,subject,recipient) 
+        Notification.create_with(action, subject, recipient, 1)
       }.to change{Notification.count}.from(0).to(1)
     end
 
     it 'should only create if it is unique' do
       expect {
-        Notification.create_with(action,subject,recipient)
-        Notification.create_with(action,subject,recipient)
+        Notification.create_with(action, subject, recipient, 1)
+        Notification.create_with(action, subject, recipient, 1)
       }.to change{Notification.count}.from(0).to(1)
     end
 
     it 'should raise if one of the required arguments are missing' do
       expect {
-        Notification.create_with(action,subject,nil)
+        Notification.create_with(action, subject, nil, 1)
       }.to raise_exception
     end
   end
