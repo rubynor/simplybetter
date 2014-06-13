b42034c85a5684d483a9573d8071f0fc471c928f
Hilde Vestøl <hilde@rubynor.com>
2014-06-13 16:38:17 +0200

    Fix spec

--- spec/models/concerns/notification_creator_spec.rb
+++ spec/models/concerns/notification_creator_spec.rb
@@ -4,20 +4,21 @@ describe NotificationCreator do
   let(:user) { double("user") }
   let(:subject) { double("subject") }
   let(:action) { double("action") }
+  let(:app) { double('app')}
 
   context 'normal notification' do
     describe 'notify_group' do
       it 'should create notifications' do
         allow(action).to receive(:creator) { double("user") }
-        expect(Notification).to receive(:create_with).with(action,subject,user).twice
-        n = NotificationCreator.new(action, subject)
+        expect(Notification).to receive(:create_with).with(action, subject, user, app).twice
+        n = NotificationCreator.new(action, subject, app)
         n.notify_group([user,user])
       end
 
       it 'should not create notification if receiver equals action.creator' do
         allow(action).to receive(:creator) { user }
-        expect(Notification).not_to receive(:create_with).with(action,subject,user)
-        n = NotificationCreator.new(action,subject)
+        expect(Notification).not_to receive(:create_with).with(action, subject, user, app)
+        n = NotificationCreator.new(action, subject, app)
         n.notify_group([user])
       end
     end
@@ -31,15 +32,15 @@ describe NotificationCreator do
       it 'should create notifications, even if receiver equals action.creator' do
         allow(action).to receive(:creator) { user }
         expect(Notification).not_to receive(:create_with).with(action,subject,user)
-        expect(Notification).to receive(:create_with).with(action,subject,user,action_attribute,action_attribute_changer).once
-        n = NotificationCreator.new(action,subject,action_attribute,action_attribute_changer)
+        expect(Notification).to receive(:create_with).with(action, subject, user, app, action_attribute, action_attribute_changer).once
+        n = NotificationCreator.new(action, subject, app, action_attribute, action_attribute_changer)
         n.notify_group([user])
       end
 
       it 'should not create notification for the user who changed the attribute and triggered notification' do
         user = action_attribute_changer
         expect(Notification).to_not receive(:create_with)
-        n = NotificationCreator.new(action,subject,action_attribute,user)
+        n = NotificationCreator.new(action, subject, app, action_attribute ,user)
         n.notify_group([user])
       end
     end
