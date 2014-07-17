4f5078f0d7ddbe903f3c4b3cb315345d769de232
Hilde Vestol <hilde@rubynor.com>
2014-07-17 16:42:52 +0200

    Remove all old widget files

--- app/assets/javascripts/backbone/widget/views/sb_utils.js
+++
@@ -1,8 +0,0 @@
-SimplyBetterApplication.Utils = {
-    userSignedIn: function(){
-        if (window.SimplyBetterApplication.config.userEmail){
-          return true;
-        }
-        return false;
-    }
-}
