a0a7958b3c8269771e95a0eaba0667275c9701d0
Hilde Vestol <hilde@rubynor.com>
2014-07-18 15:44:38 +0200

    Remove unused actions, routes and methods

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,6 +1,6 @@
 class IdeasController < ApplicationController
-  before_action :set_application, only: [:add_to_group, :remove_from_group, :update, :destroy, :new, :create, :index]
-  before_action :set_idea, except: [:new, :create, :index]
+  before_action :set_application, only: [:update, :destroy, :index]
+  before_action :set_idea, only: [:update, :destroy]
 
   def index
     if current_customer
@@ -8,53 +8,14 @@ class IdeasController < ApplicationController
     end
   end
 
-  def new
-    @idea = Idea.new
-  end
-
-  def show
-    render template: 'ideas/show'
-  end
-
-  def create
-    @idea = Idea.new(idea_attributes)
-    @idea.application = @application
-    @idea.creator = current_customer
-    respond_to do |format|
-      if @idea.save
-        format.html { redirect_to new_application_idea_path(@application.id), notice: 'Idea was successfully created.' }
-      else
-        format.html { render action: :edit }
-      end
-    end
-  end
-
   def update
-    if @idea.save_and_notify(idea_attributes, current_customer)
-      respond_to do |format|
-        format.json{ show }
-        format.html{
-          redirect_to administrate_group_application_path(@application.id),
-            notice: 'Idea was updated'
-        }
-      end
-    else
-      respond_to do |format|
-        format.html { render :edit }
-        format.json { show }
-      end
-    end
+   @idea.save_and_notify(idea_attributes, current_customer)
+   render template: 'ideas/show'
   end
 
   def destroy
     @idea.destroy!
-    respond_to do |format|
-      format.json { render json: { message: 'success' } }
-      format.html {
-        redirect_to administrate_group_application_path(@application.id),
-                    notice: 'Idea was removed'
-      }
-    end
+    render json: { message: 'success' }
   end
 
   private
@@ -71,9 +32,5 @@ class IdeasController < ApplicationController
     if current_customer
       @idea ||= set_application.ideas.find(params[:id])
     end
-  end
-
-  def push_string(object,model_name)
-    ["#{object.name} #{object.email}", "#{model_name},#{object.id}"]
   end
 end
