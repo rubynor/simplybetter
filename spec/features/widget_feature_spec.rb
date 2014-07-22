e2041df5a56da041917901fca1932cb3ba453b7a
Hilde Vestol <hilde@rubynor.com>
2014-07-22 16:23:28 +0200

    Use url instead of iframe for feature tests

--- spec/features/widget_feature_spec.rb
+++ spec/features/widget_feature_spec.rb
@@ -12,64 +12,50 @@ describe WidgetController, js: true do
       fill_in 'email', with: 'lol@lol.com'
       fill_in 'password', with: 'dev'
       click_button 'Sign in'
-      page.find('#simplybetterActivateButton').click
+      click_link 'Go to app'
     end
     example 'comment an idea' do
       expect do
-        page.within_frame 'simplybetterIframe' do
-          page.should have_content 'Test'
-          first('h1', text: 'Test').click
-          fill_in 'Leave a comment...', with: 'My comment'
-          click_button 'OK'
-          page.should have_content('My comment')
-        end
+        page.should have_content 'Test'
+        first('h1', text: 'Test').click
+        fill_in 'Leave a comment...', with: 'My comment'
+        click_button 'OK'
+        page.should have_content('My comment')
       end.to change(Application.last.ideas.first.comments, :count).by(1)
     end
     example 'new idea' do
       expect do
-        page.within_frame 'simplybetterIframe' do
-          fill_in 'title-input', with: 'Idea'
-          fill_in 'Description', with: 'My idea description'
-          click_button 'send'
-          page.should have_css('.comment-body')
-        end
+        fill_in 'title-input', with: 'Idea'
+        fill_in 'Description', with: 'My idea description'
+        click_button 'send'
+        page.should have_css('.comment-body')
       end.to change(Application.last.ideas, :count).by(1)
     end
     example 'up vote' do
-      page.within_frame 'simplybetterIframe' do
-        first('.up').click
-      end
+      first('.up').click
       expect(Application.last.ideas.first.votes_count).to eq(1)
     end
     example 'down vote' do
-      page.within_frame 'simplybetterIframe' do
-        first('.down').click
-      end
+      first('.down').click
       expect(Application.last.ideas.first.votes_count).to eq(-1)
     end
     example 'open notifications' do
-      page.within_frame 'simplybetterIframe' do
-        page.should_not have_content 'Notifications'
-        first('.new-notifications').click
-        page.should have_content 'Notifications'
-      end
+      page.should_not have_content 'Notifications'
+      first('.new-notifications').click
+      page.should have_content 'Notifications'
     end
     example 'mark notification as read' do
       Notification.last.checked.should eq(nil)
-      page.within_frame 'simplybetterIframe' do
-        page.should_not have_content 'Notifications'
-        first('.new-notifications').click
-        first('.new').click
-      end
+      page.should_not have_content 'Notifications'
+      first('.new-notifications').click
+      first('.new').click
       Notification.last.checked.should eq(true)
     end
     example 'navigation' do
-      page.within_frame 'simplybetterIframe' do
-        first('h1', text: 'Test').click
-        page.should have_content 'BACK'
-        first('.back-arrow').click
-        page.should_not have_content 'BACK'
-      end
+      first('h1', text: 'Test').click
+      page.should have_content 'BACK'
+      first('.back-arrow').click
+      page.should_not have_content 'BACK'
     end
   end
 
