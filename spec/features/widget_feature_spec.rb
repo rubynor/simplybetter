f3e3cd6f6874ab6c7518457c7925f59f185d5596
Hilde Vestol <hilde@rubynor.com>
2014-07-22 16:40:44 +0200

    Add some should have

--- spec/features/widget_feature_spec.rb
+++ spec/features/widget_feature_spec.rb
@@ -32,26 +32,34 @@ describe WidgetController, js: true do
       end.to change(Application.last.ideas, :count).by(1)
     end
     example 'up vote' do
+      page.should have_css '.up'
       first('.up').click
+      page.should have_css '.active'
       expect(Application.last.ideas.first.votes_count).to eq(1)
     end
     example 'down vote' do
+      page.should have_css '.down'
       first('.down').click
+      page.should have_css '.active'
       expect(Application.last.ideas.first.votes_count).to eq(-1)
     end
     example 'open notifications' do
       page.should_not have_content 'Notifications'
+      page.should have_css '.new-notifications'
       first('.new-notifications').click
       page.should have_content 'Notifications'
     end
     example 'mark notification as read' do
       Notification.last.checked.should eq(nil)
       page.should_not have_content 'Notifications'
+      page.should have_css '.new-notifications'
       first('.new-notifications').click
+      page.should have_css '.new'
       first('.new').click
       Notification.last.checked.should eq(true)
     end
     example 'navigation' do
+      page.should have_content 'Test'
       first('h1', text: 'Test').click
       page.should have_content 'BACK'
       first('.back-arrow').click
