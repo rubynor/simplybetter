d2ae82871c42bdcf0b284e9d2b0c0f32f04e22da
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2014-07-23 12:27:52 +0200

    typo2

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -97,7 +97,7 @@ SimplyBetter::Application.configure do
 
   class WidgetPath
     def self.path
-      "simplybetter-staging.herokuapp.com"
+      "http://simplybetter-staging.herokuapp.com"
     end
   end
 end
