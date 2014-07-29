2dd12b8a022f7cfed5d664ff82be5a647088772f
Hilde Vestol <hilde@rubynor.com>
2014-07-29 15:48:39 +0200

    Refactor

--- app/controllers/concerns/creator_finder.rb
+++ app/controllers/concerns/creator_finder.rb
@@ -1,21 +1,17 @@
 module CreatorFinder
-  def get_current_user(application,user_email)
+  def get_current_user(application, user_email)
     if application && user_email
       @current_user ||= creator(application, user_email)
     else
-      raise "Missing one of the arguments application: #{application}, or user_email: #{user_email}"
+      fail ArgumentError "Missing one of the arguments application: #{application}, or user_email: #{user_email}"
     end
   end
 
   def creator(application, creator_email)
     a_customer = Customer.find_by(email: creator_email)
+    return a_customer if a_customer
     user = User.find_by(email: creator_email)
-    if a_customer
-      return a_customer
-    elsif user
-      return user
-    else
-      raise "This user does not exist"
-    end
+    return user if user
+    fail 'This user does not exist'
   end
 end
