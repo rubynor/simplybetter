24c597f12768a3f7caf2155835f2adf54de3e6eb
Pål Andre Sundt <pal@rubynor.com>
2014-07-30 11:53:34 +0200

    Add ui bootstrap

--- app/assets/javascripts/widget.js
+++ app/assets/javascripts/widget.js
@@ -2,6 +2,8 @@
 //= require angular-1.2.18/angular
 //= require angular-1.2.18/angular-resource
 //= require angular-1.2.18/angular-route
+//= require angular-1.2.18/ui-bootstrap-custom-0.10.0
+//= require angular-1.2.18/ui-bootstrap-custom-tpls-0.10.0
 //= require angular/widget/directives/simply_directives
 //= require angular/widget
 //= require_tree ./angular/widget
