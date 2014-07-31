e474c5acd0606790680b46d76dc8f43a123352de
Hilde Vestol <hilde@rubynor.com>
2014-07-31 16:42:00 +0200

    Fix comment typo

--- app/controllers/widget_controller.rb
+++ app/controllers/widget_controller.rb
@@ -17,9 +17,9 @@ class WidgetController < ApplicationController
 
   def create_user
     customer = Customer.find_by(email: params[:email])
-    return if customer # Need to do more lookups if customer found
+    return if customer # No need to do more lookups if customer found
     user = User.find_by(email: params[:email])
-    return if user # Need to do more lookups if user found
+    return if user # No need to do more lookups if user found
 
     app = Application.find_by(token: params[:appkey])
     app.users.create(email: params[:email], name: params[:name])
