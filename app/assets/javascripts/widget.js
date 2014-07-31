aa0042e00723e489dbb5ee4b7f0722841aebe98e
Pål Andre Sundt <pal@rubynor.com>
2014-07-31 17:02:38 +0200

    Add named-routes provider to get reverse routing

--- app/assets/javascripts/widget.js
+++ app/assets/javascripts/widget.js
@@ -4,6 +4,7 @@
 //= require angular-1.2.18/angular-route
 //= require angular-1.2.18/ui-bootstrap-custom-0.10.0
 //= require angular-1.2.18/ui-bootstrap-custom-tpls-0.10.0
+//= require angular-1.2.18/named-routes
 //= require angular/widget/directives/simply_directives
 //= require angular/widget
 //= require_tree ./angular/widget
