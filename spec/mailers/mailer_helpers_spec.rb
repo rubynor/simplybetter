56a979390a22518f44ef065d0b2497ae30bdcbea
Pål Andre Sundt <pal@rubynor.com>
2014-08-05 11:36:50 +0200

    Add specs for MailerHelpers

---
+++ spec/mailers/mailer_helpers_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe MailerHelpers do
+  class Dummy
+    include MailerHelpers
+  end
+
+  subject { Dummy.new }
+
+  describe '.should_send_mail?' do
+    context 'user matches the person who triggered the notification' do
+      it 'should return false' do
+        user = User.make
+        expect(subject.should_send_mail?(user, user)).to be_false
+      end
+    end
+
+    context 'user has unsubscribed from emails' do
+      it 'should return false' do
+        user = User.make!
+        user.email_setting.update_attributes(unsubscribed: true)
+        expect(subject.should_send_mail?(user, User.make)).to be_false
+      end
+    end
+
+    it 'should return true' do
+      expect(subject.should_send_mail?(User.make, User.make)).to be_true
+    end
+  end
+end
