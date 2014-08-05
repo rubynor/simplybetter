e1d931c701feb441541f3e0d003c5b3aac79ff0f
Pål Andre Sundt <pal@rubynor.com>
2014-08-05 16:42:57 +0200

    Set a token after create and create method get_or_set_unsubscribe_token

--- app/models/email_setting.rb
+++ app/models/email_setting.rb
@@ -1,7 +1,21 @@
 class EmailSetting < ActiveRecord::Base
   belongs_to :mailable, polymorphic: true
 
+  before_create :set_email_unsubscribe_token
+
+  def get_or_set_unsubscribe_token
+    unless unsubscribe_token
+      set_email_unsubscribe_token()
+      save!
+    end
+    self.unsubscribe_token
+  end
+
   def self.unsubscribed?(mailable)
     find_or_create_by(mailable: mailable).unsubscribed
+  end
+
+  def set_email_unsubscribe_token
+    self.unsubscribe_token = SecureRandom.urlsafe_base64
   end
 end
