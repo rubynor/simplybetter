5a64f7021a0ef5a34b78232fbd65ca5ea328d720
Pål Andre Sundt <pal@rubynor.com>
2014-08-05 09:38:33 +0200

    Use rails own find_or_create_by

--- app/models/email_setting.rb
+++ app/models/email_setting.rb
@@ -1,11 +1,7 @@
 class EmailSetting < ActiveRecord::Base
   belongs_to :mailable, polymorphic: true
 
-  def self.find_or_create(mailable)
-    where(mailable: mailable).first_or_create
-  end
-
   def self.unsubscribed?(mailable)
-    find_or_create(mailable).unsubscribed
+    find_or_create_by(mailable: mailable).unsubscribed
   end
 end
