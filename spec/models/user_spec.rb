99c99fd705914dac8996271a303d9ad8e6f84a92
Hilde Vestol <hilde@rubynor.com>
2014-08-05 17:06:14 +0200

    Add model spec for user and remove unusued method and relation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :all do
+    @user = User.make!
+  end
+  let(:valid_attributes) do
+    {
+        email: 'lol@lol.com'
+    }
+  end
+
+  describe 'validations' do
+    subject { User.new(valid_attributes) }
+    it { should_not have_valid(:email).when(nil) }
+    it { should_not have_valid(:email).when(@user.email) }
+    it { should_not have_valid(:email).when('lol.com') }
+  end
+
+  describe 'relations' do
+    it 'should have many widgets' do
+      t = User.reflect_on_association(:widgets)
+      t.macro.should == :has_many
+    end
+  end
 end
