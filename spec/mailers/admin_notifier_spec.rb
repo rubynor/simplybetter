a95020868faf9979f3210132f492fb9b14e08bfc
Pål Andre Sundt <pal@rubynor.com>
2014-08-05 11:19:11 +0200

    Create spec for send_to_group

--- spec/mailers/admin_notifier_spec.rb
+++ spec/mailers/admin_notifier_spec.rb
@@ -1,5 +1,15 @@
 require "spec_helper"
 
 describe AdminNotifier do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.send_to_group' do
+    let(:users) { [User.make, User.make, User.make] }
+
+    it 'should call new_idea for each user in the group' do
+      notifier = AdminNotifier.send(:new)
+      notifier.stub(:should_send_mail?) { true }
+      notifier.stub_chain(:new_idea, :deliver) { true }
+      expect(notifier).to receive(:new_idea).exactly(3).times
+      notifier.send_to_group(users, User.make, Idea.make)
+    end
+  end
 end
