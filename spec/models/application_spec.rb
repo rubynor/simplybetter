32e0940d1617edc7a94eb8a39c883d9f4db2d9f2
Hilde Vestol <hilde@rubynor.com>
2014-08-07 15:46:49 +0200

    Add some application spec

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,5 +1,40 @@
 require 'spec_helper'
 
 describe Application do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before :all do
+    @application = Application.make!
+  end
+
+  let(:valid_attributes){ { name: 'My application' } }
+
+  describe 'validations' do
+    subject { Application.new(valid_attributes) }
+    it { should_not have_valid(:name).when(nil) }
+    it { should_not have_valid(:token).when(@application.token) }
+  end
+
+  describe 'should have many' do
+    after :each do
+      @reflection.macro.should == :has_many
+    end
+    it 'ideas' do
+      @reflection = Application.reflect_on_association(:ideas)
+    end
+    it 'comments' do
+      @reflection = Application.reflect_on_association(:comments)
+    end
+    it 'notifications' do
+      @reflection = Application.reflect_on_association(:notifications)
+    end
+    it 'customers' do
+      @reflection = Application.reflect_on_association(:customers)
+    end
+    it 'users' do
+      @reflection = Application.reflect_on_association(:users)
+    end
+    it 'widget_customers' do
+      @reflection = Application.reflect_on_association(:widget_customers)
+    end
+  end
 end
