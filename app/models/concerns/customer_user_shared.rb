12f2af86b6dd6f0772b83075010f7180847795e7
Hilde Vestol <hilde@rubynor.com>
2014-08-08 17:05:03 +0200

    Add email_setting after create

---
+++ app/models/concerns/customer_user_shared.rb
@@ -0,0 +1,25 @@
+module CustomerUserShared
+  extend ActiveSupport::Concern
+  included do
+    has_many :comments, as: :creator, inverse_of: :creator
+    has_many :ideas, as: :creator
+    has_many :votes, as: :voter
+    has_one :email_setting, as: :mailable, dependent: :destroy
+
+    after_create :create_email_setting
+
+    validates :email,
+              format: {
+                  with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+                  on: :create},
+              uniqueness: true,
+              presence: true
+
+    private
+
+    def create_email_setting
+      EmailSetting.create!(mailable: self)
+    end
+
+  end
+end
