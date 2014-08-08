de88f11142f17b75629bbae701f90bd16d5365aa
Pål Andre Sundt <pal@rubynor.com>
2014-08-08 10:09:48 +0200

    Remove .get_or_set_unsubscribe_token and add class method unsubscribe

--- app/models/email_setting.rb
+++ app/models/email_setting.rb
@@ -3,16 +3,22 @@ class EmailSetting < ActiveRecord::Base
 
   before_create :set_email_unsubscribe_token
 
-  def get_or_set_unsubscribe_token
-    unless unsubscribe_token
-      set_email_unsubscribe_token()
-      save!
-    end
-    self.unsubscribe_token
-  end
-
   def self.unsubscribed?(mailable)
     find_or_create_by(mailable: mailable).unsubscribed
+  end
+
+  def self.unsubscribe!(unsubscribe_token)
+    email_setting = find_by(unsubscribe_token: unsubscribe_token)
+    unless email_setting
+      raise ArgumentError, "Could not find an email setting based on this unsubscribe_token: #{unsubscribe_token}"
+    end
+
+    if email_setting.unsubscribed?
+      "You are already unsubscribed from email notifications"
+    else
+      email_setting.update_attributes!(unsubscribed: true)
+      "You have been unsubscribed from email notifications"
+    end
   end
 
   def set_email_unsubscribe_token
