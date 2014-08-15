68f018c74d7ea49856de3ac8a9d735045ea57dcd
Pål Andre Sundt <pal@rubynor.com>
2014-08-15 15:25:43 +0200

    Add helpful text

--- app/models/concerns/abuse_reportable.rb
+++ app/models/concerns/abuse_reportable.rb
@@ -1,6 +1,8 @@
 module AbuseReportable
   extend ActiveSupport::Concern
 
+  # Please include a reference to application from the model
+  # you're including this in
   included do
     has_many :abuse_reports, as: :reportable, dependent: :destroy
   end
