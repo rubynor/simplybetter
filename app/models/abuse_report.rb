bfe88df7cae089788e4ee19ca9b5865265dbef18
Pål Andre Sundt <pal@rubynor.com>
2014-08-22 17:14:03 +0200

    Add inverse relations

--- app/models/abuse_report.rb
+++ app/models/abuse_report.rb
@@ -1,8 +1,9 @@
 class AbuseReport < ActiveRecord::Base
-  belongs_to :reportable, polymorphic: true
-  belongs_to :reporter, polymorphic: true
+  belongs_to :abuse_reportable, polymorphic: true, inverse_of: :abuse_reports
+  belongs_to :abuse_reporter, polymorphic: true, inverse_of: :abuse_reports
+  belongs_to :application
 
-  validates_presence_of :reportable, :reporter
+  validates_presence_of :abuse_reportable, :abuse_reporter, :application
 
-  scope :for, ->(application) { where(application: application) }
+  scope :for, ->(application) { where(application_id: application.id) }
 end
