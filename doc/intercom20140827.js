d19d119fdf7ccd1f5f91208d834cc6924cdcfee6
Ole Morten Amundsen <ole.morten.amundsen@gmail.com>
2014-09-05 15:55:42 +0200

    how intercom does it with their include script

---
+++ doc/intercom20140827.js
@@ -0,0 +1,12 @@
+<script>
+  window.intercomSettings = {
+    // TODO: The current logged in user's full name
+    name: "John Doe",
+    // TODO: The current logged in user's email address.
+    email: "john.doe@example.com",
+    // TODO: The current logged in user's sign-up date as a Unix timestamp.
+    created_at: 1234567890,
+    app_id: "r4o2zym6"
+};
+</script>
+<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/r4o2zym6';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
