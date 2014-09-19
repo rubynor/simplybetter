34bfc1bafb8f6681075dff029c3c77c5d00fdd36
Pål Andre Sundt <pal@rubynor.com>
2014-09-19 09:50:06 +0200

    Login after password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -19,6 +19,7 @@ class PasswordResetsController < ApplicationController
     if @customer.password_reset_sent_at < 2.hours.ago
       redirect_to new_password_reset_path, alert: 'Password reset has expired.'
     elsif @customer.update_attributes(password: params[:customer][:password], password_confirmation: params[:customer][:password_confirmation])
+      cookies[:auth_token] = @customer.auth_token
       redirect_to root_url, notice: 'Password has been reset.'
     else
       render :edit
