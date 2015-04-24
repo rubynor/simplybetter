9cef1309a53f81b29fad22a61d75f92ae149865e
Hilde Vestol <hilde@rubynor.com>
2015-04-24 17:21:24 +0200

    Refactor

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -13,7 +13,6 @@ shared_context 'unauthorized' do
 
     before do
       @idea = Idea.make!(application: application, completed: false)
-      @idea.subscribe
     end
 
     it 'it should not update' do
@@ -46,27 +45,26 @@ describe IdeasController do
     end
 
     describe 'PUT update' do
+      let(:completed) { true }
+      subject { put :update, application_id: @application.id, id: @idea.to_param, idea: { completed: completed}, format: :json }
       before do
         @idea = Idea.make!(application: @application, completed: false)
         @idea.subscribe
       end
+
       describe 'set complete' do
         it 'sets completed to true' do
-          expect do
-            put :update, application_id: @application.id, id: @idea.to_param, idea: { completed: true}, format: :json
-          end.to change{ Idea.last.completed }.from(false).to(true)
+          expect { subject }.to change{ Idea.last.completed }.from(false).to(true)
         end
         it 'notifies idea owner' do
-          expect do
-            put :update, application_id: @application.id, id: @idea.to_param, idea: { completed: true}, format: :json
-          end.to change(Notification, :count).by(1)
+          expect { subject }.to change(Notification, :count).by(1)
         end
       end
+
       it 'should not notify if completed changed to false' do
         @idea.update_attributes! completed: true
-        expect do
-          put :update, application_id: @application.id, id: @idea.to_param, idea: { completed: false}, format: :json
-        end.to change(Notification, :count).by(0)
+        completed = false
+        expect { subject }.to change(Notification, :count).by(0)
       end
     end
   end
