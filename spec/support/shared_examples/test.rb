ca4c4021efeee0e7a4652027b3baa8ad347c4b7a
Hilde Vestol <hilde@rubynor.com>
2015-04-24 17:54:10 +0200

    Delete unused file

--- spec/support/shared_examples/test.rb
+++
@@ -1,6 +0,0 @@
-# shared_context 'test' do
-#   it 'returns unauthorized' do
-#     get :index, application_id: @application.id, format: :json
-#     expect(response.status).to eq(200)
-#   end
-# end
