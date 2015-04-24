5ef63dcbbe1549361f8e0c4a66354b46d8b35469
Hilde Vestol <hilde@rubynor.com>
2015-04-24 16:36:08 +0200

    Refactor: use shared_context

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -1,10 +1,37 @@
 require 'spec_helper'
 
+shared_context 'unauthorized' do
+  describe 'index' do
+    it 'response unauthorized' do
+      get :index, application_id: application.id, format: :json
+      expect(response.status).to eq(401)
+    end
+  end
+
+  describe 'PUT update' do
+    subject { put :update, application_id: application.id, id: @idea.to_param, idea: { completed: true}, format: :json }
+
+    before do
+      @idea = Idea.make!(application: application, completed: false)
+      @idea.subscribe
+    end
+
+    it 'it should not update' do
+      expect{subject}.to_not change{ Idea.last.completed }
+    end
+
+    it 'shuold respond with unauthorized' do
+      subject
+      expect(response.status).to eq(401)
+    end
+  end
+end
+
+
 describe IdeasController do
   include SessionHelper
 
   describe 'logged in' do
-
     before do
       @customer = Customer.make!
       @application = Application.make!(customers: [@customer])
@@ -44,66 +71,17 @@ describe IdeasController do
     end
   end
 
-  describe 'not authorized' do
+  describe 'not signed in' do
+    let(:application) { Application.make! }
+    it_behaves_like 'unauthorized'
+  end
+
+  describe 'signed in, but not access to app' do
     before do
       @customer = Customer.make!
-      @application = Application.make!
     end
-
-    describe 'not signed in' do
-      describe 'index' do
-        it 'should assign return unauthorized' do
-          get :index, application_id: @application.id, format: :json
-          expect(response.status).to eq(401)
-          expect(assigns(:application)).to eq(nil)
-          expect(assigns(:ideas)).to eq(nil)
-        end
-      end
-
-      describe 'PUT update' do
-        before do
-          @idea = Idea.make!(application: @application, completed: false)
-          @idea.subscribe
-        end
-        describe 'set complete' do
-          it 'it should not update' do
-            expect do
-              put :update, application_id: @application.id, id: @idea.to_param, idea: { completed: true}, format: :json
-            end.to_not change{ Idea.last.completed }
-            expect(response.status).to be(401)
-          end
-        end
-      end
-    end
-
-    describe 'signed in, but no access to app' do
-      before do
-        sign_in_customer(@customer)
-      end
-
-      describe 'index' do
-        it 'should assign return unauthorized' do
-          get :index, application_id: @application.id, format: :json
-          expect(response.status).to eq(401)
-          expect(assigns(:application)).to eq(nil)
-          expect(assigns(:ideas)).to eq(nil)
-        end
-      end
-
-      describe 'PUT update' do
-        before do
-          @idea = Idea.make!(application: @application, completed: false)
-          @idea.subscribe
-        end
-        describe 'set complete' do
-          it 'it should not update' do
-            expect do
-              put :update, application_id: @application.id, id: @idea.to_param, idea: { completed: true}, format: :json
-            end.to_not change{ Idea.last.completed }
-            expect(response.status).to be(401)
-          end
-        end
-      end
-    end
+    let(:application) { Application.make!(customers: [@customer]) }
+    it_behaves_like 'unauthorized'
   end
+
 end
---
+++ spec/support/shared_examples/test.rb
@@ -0,0 +1,6 @@
+# shared_context 'test' do
+#   it 'returns unauthorized' do
+#     get :index, application_id: @application.id, format: :json
+#     expect(response.status).to eq(200)
+#   end
+# end
