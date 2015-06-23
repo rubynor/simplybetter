486c04a1915b0e5cf6b3caf2e43c8b6784a3e489
Hilde Vestol <hilde@rubynor.com>
2015-06-23 14:38:26 +0200

    Migrate!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150513115931) do
+ActiveRecord::Schema.define(version: 20150505095057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
