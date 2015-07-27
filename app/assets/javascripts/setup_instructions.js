f52853ac7688fdeeded429202507d31402a78bdb
Pål Andre Sundt <pal@rubynor.com>
2015-07-27 17:59:37 +0200

    Add slight delay on icon select to make sure the icon has been selected before submitting

--- app/assets/javascripts/setup_instructions.js
+++ app/assets/javascripts/setup_instructions.js
@@ -3,9 +3,10 @@ $(function(){
     if (SBTools.isPresent($('body.applications.show'))){
         hljs.initHighlightingOnLoad();
 
-        //TODO OMA: When is this needed? Should be for something more specific than 'label'. On reading this, I'm not sure what the target is.
-        $('label').on('click', function(e){
-          $(e.target).closest('form').submit();
+        $('form.icon_select label').on('click', function(e){
+          setTimeout( function () {
+            $(e.target).closest('form').submit();
+          }, 50 );
         });
 
     }
