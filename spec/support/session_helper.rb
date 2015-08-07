3c7d1b18164210cdc676755d00a2951836983e88
Hilde Vestol <hilde@rubynor.com>
2015-08-07 12:18:45 +0200

    Add comments specs with info param

--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -3,4 +3,8 @@ module SessionHelper
   def sign_in_customer(customer)
     cookies[:auth_token] = customer.auth_token
   end
+
+  def decode_to_base64(appkey, email, name)
+    Base64.encode64("?appkey=#{appkey}&email=#{email}&name=#{name}")
+  end
 end
