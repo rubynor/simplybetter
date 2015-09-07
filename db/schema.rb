5193f02564d0135040e4c7738cd0143593ad925f
Pål Andre Sundt <pal@rubynor.com>
2015-09-07 17:09:11 +0200

    Add a unique index to applications_customers and remove the uniq scope

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150810075908) do
+ActiveRecord::Schema.define(version: 20150828121853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20150810075908) do
     t.integer "application_id"
   end
 
-  add_index "applications_customers", ["application_id", "customer_id"], name: "index_applications_customers_on_application_id_and_customer_id", using: :btree
+  add_index "applications_customers", ["application_id", "customer_id"], name: "index_applications_customers_on_application_id_and_customer_id", unique: true, using: :btree
   add_index "applications_customers", ["customer_id"], name: "index_applications_customers_on_customer_id", using: :btree
 
   create_table "comments", force: true do |t|
