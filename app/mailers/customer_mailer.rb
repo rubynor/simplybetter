31e1768e4e08943bc6e086a3ef64efc34497b2da
Pål Andre Sundt <pal@rubynor.com>
2015-09-08 12:17:23 +0200

    Add invite customer email methods

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -5,4 +5,12 @@ class CustomerMailer < ActionMailer::Base
     @user = user
     mail to: user.email, subject: 'Password Reset'
   end
+
+  def invite(customer)
+    mail to: customer.email, subject: 'You have been added to a team'
+  end
+
+  def invite_new_customer(customer, password)
+    mail to: customer.email, subject: 'You have been added to a team'
+  end
 end
