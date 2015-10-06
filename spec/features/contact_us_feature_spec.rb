7ce4d1bd308e2f5ec1f55653a4e9f5cc55a8d1c5
Hilde Vestol <hilde@rubynor.com>
2015-10-06 14:20:22 +0200

    Fix spec and add contact us feature spec

---
+++ spec/features/contact_us_feature_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe 'ContactUs', js: true, type: :feature do
+
+  it 'sends a mail with message' do
+    visit root_path
+    fill_in 'name', with: 'My name'
+    fill_in 'email', with: 'my@email.com'
+    fill_in 'message', with: 'my message'
+    click_button 'SUBMIT'
+    expect(page).to have_content('Thank you for your message, we will return to you as soon as possible')
+  end
+end
