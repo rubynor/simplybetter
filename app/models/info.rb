a1d6e3757307608de56966bcd88544f7a2cde68a
Hilde Vestol <hilde@rubynor.com>
2015-10-28 14:26:33 +0100

    Add helper for customer notifications

--- app/models/info.rb
+++ app/models/info.rb
@@ -3,6 +3,17 @@ class Info < ActiveRecord::Base
 
   validates :body, :route, presence: true
 
+  # This metod is a helper to create notifications to customers
+  def self.create_and_notify_customers(text:, route:, external: false)
+    info = Info.create!(body: text, route: route, external: external)
+    sb = Customer.find_by_email('support@simplybetter.io')
+    Application.all.each do |app|
+      app.customers.each do |c|
+        Notification.create!(subject: info, action: info, recipient: c, action_attribute_changed_by: sb, application_id: app.id)
+      end
+    end
+  end
+
   def notification_text(_recipient, **_args)
     [ { bold: 'Info:', normal: body, url: 'settings' } ]
   end
