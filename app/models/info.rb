adf05fc8ffda726a8d92632d1b1fc6d7731c254a
Hilde Vestol <hilde@rubynor.com>
2015-10-28 14:03:20 +0100

    Add validation

--- app/models/info.rb
+++ app/models/info.rb
@@ -1,6 +1,8 @@
 class Info < ActiveRecord::Base
   has_many :notifications, as: :subject, dependent: :destroy
 
+  validates :body, :route, presence: true
+
   def notification_text(_recipient, **_args)
     [ { bold: 'Info:', normal: body, url: 'settings' } ]
   end
