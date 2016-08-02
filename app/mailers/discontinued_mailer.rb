c6f74e6a7402d658c9bb881dae120de98347e19e
Hilde Vestol <hilde@rubynor.com>
2016-08-02 11:46:37 +0200

    Change closing date

--- app/mailers/discontinued_mailer.rb
+++ app/mailers/discontinued_mailer.rb
@@ -2,7 +2,7 @@ class DiscontinuedMailer < ActionMailer::Base
   default from: 'support@simplybetter.io'
 
   def send_to_all
-    subject = 'Simplybetter discontinued'
+    subject = 'Simplybetter will close the site Oct 1st'
 
     mail to: 'support@simplybetter.io', bcc: Customer.all.map(&:email), subject: subject
   end
